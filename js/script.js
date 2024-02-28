c4417cf66a1c3a16bbe656a42db53ec7732d67ca
Urszula Majcher <ulunka.m@gmail.com>
2024-02-28 23:34:07 +0100

    added function form & addNew task

--- js/script.js
+++ js/script.js
@@ -29,10 +29,27 @@
     document.querySelector(".js-tasks").innerHTML = tasksListContent;
   };
 
+  const addNewTask = (newTask) => {
+    tasks.push({ content: newTask });
+
+    render();
+  };
+
   const init = () => {
     render();
 
     const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const newTask = document.querySelector(".js-newTask").value.trim();
+
+      if (newTask === "") {
+        return;
+      }
+      addNewTask(newTask);
+    });
   };
 
   init();
